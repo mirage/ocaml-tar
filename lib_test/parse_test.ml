2369a979f266d42de5d735ecd0a34f1667762436
David Scott <dave.scott@eu.citrix.com>
2013-10-03 14:49:15 +0100

    Add 'make test'
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

---
+++ lib_test/parse_test.ml
@@ -0,0 +1,32 @@
+(*
+ * Copyright (C) 2011-2013 Citrix Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+open OUnit
+
+let create () =
+  ()
+
+
+let _ =
+  let verbose = ref false in
+  Arg.parse [
+    "-verbose", Arg.Unit (fun _ -> verbose := true), "Run in verbose mode";
+  ] (fun x -> Printf.fprintf stderr "Ignoring argument: %s" x)
+    "Test tar parser";
+
+  let suite = "tar" >:::
+    [
+      "create" >:: create;
+     ] in
+  run_test_tt ~verbose:!verbose suite
+
