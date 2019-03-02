aee659ca500d88b7d5ea86eaf595d61869de7f1c
Hannes Mehnert <hannes@mehnert.org>
2019-03-02 16:39:58 +0100

    mirage-kv 2.0.0

--- lib_test/parse_test.ml
+++ lib_test/parse_test.ml
@@ -196,12 +196,7 @@ module Test(B: BLOCK) = struct
            KV_RO.connect b >>= fun k ->
            Lwt_list.iter_s
              (fun file ->
-                KV_RO.size k file >>= function
-                | Error e ->
-                  failwith ("KV_RO: expected ok, got Error " ^ (Fmt.to_to_string KV_RO.pp_error e))
-                | Ok size ->
                 let stats = Unix.LargeFile.stat file in
-                assert_equal ~printer:Int64.to_string stats.Unix.LargeFile.st_size size;
                 let read_file key ofs len =
                   let fd = Unix.openfile key [ Unix.O_RDONLY ] 0 in
                   finally
@@ -212,22 +207,20 @@ module Test(B: BLOCK) = struct
                        assert_equal ~printer:string_of_int len len';
                        Bytes.to_string buf
                     ) (fun () -> Unix.close fd) in
-                let read_tar key ofs len =
-                  KV_RO.read k key ofs len
-                  >>= function
+                let read_tar key =
+                  KV_RO.get k key >>= function
                   | Error _ -> failwith "KV_RO.read"
-                  | Ok bufs -> return (String.concat "" (List.map Cstruct.to_string bufs)) in
+                  | Ok buf -> return buf in
                 (* Read whole file *)
                 let size = stats.Unix.LargeFile.st_size in
                 let value = read_file file 0 (Int64.to_int size) in
-                read_tar file 0L size
-                >>= fun value' ->
+                read_tar (Mirage_kv.Key.v file) >>= fun value' ->
                 assert_equal ~printer:(fun x -> x) value value';
                 if Int64.compare size 2L = 1 then begin
                   let value = read_file file 1 ((Int64.to_int size) - 2) in
-                  read_tar file 1L (Int64.sub size 2L)
-                  >>= fun value' ->
-                  assert_equal ~printer:(fun x -> x) value value';
+                  read_tar (Mirage_kv.Key.v file) >>= fun value' ->
+                  let value'' = String.sub value' 1 ((Int64.to_int size) - 2) in
+                  assert_equal ~printer:(fun x -> x) value value'';
                   return ()
                 end else return ()
              ) files in
--- mirage/tar_mirage.ml
+++ mirage/tar_mirage.ml
@@ -22,31 +22,47 @@ module StringMap = Map.Make(String)
 
 module Make_KV_RO (BLOCK : Mirage_block_lwt.S) = struct
 
+  type entry =
+    | Value of Tar.Header.t * int64
+    | Dict of Tar.Header.t * entry StringMap.t
+
   type t = {
     b: BLOCK.t;
-    map: (Tar.Header.t * int64) StringMap.t;
+    map: entry;
     info: Mirage_block.info;
   }
 
-  type error = Mirage_kv.error
-
   type 'a io = 'a Lwt.t
 
-  type page_aligned_buffer = Cstruct.t
+  type key = Mirage_kv.Key.t
 
-  let pp_error = Mirage_kv.pp_error
+  type value = string
 
-  (* Compare filenames without a leading / or ./ *)
-  let trim_slash x =
-    let startswith prefix x =
-      let prefix' = String.length prefix in
-      let x' = String.length x in
-      x' >= prefix' && (String.sub x 0 prefix' = prefix) in
-    if startswith "./" x
-    then String.sub x 2 (String.length x - 2)
-    else if startswith "/" x
-    then String.sub x 1 (String.length x - 1)
-    else x
+  type error = [ Mirage_kv.error | `Block of BLOCK.error ]
+
+  let pp_error ppf = function
+    | #Mirage_kv.error as e -> Mirage_kv.pp_error ppf e
+    | `Block b -> BLOCK.pp_error ppf b
+
+  let get_node t key =
+    let rec find e = function
+      | [] -> Ok e
+      | hd::tl -> match e with
+        | Value _ -> Error (`Dictionary_expected key)
+        | Dict (_, m) -> match StringMap.find_opt hd m with
+          | Some e -> find e tl
+          | None -> Error (`Not_found key)
+    in
+    find t (Mirage_kv.Key.segments key)
+
+  let exists t key =
+    let r = match get_node t.map key with
+      | Ok (Value _) -> Ok (Some `Value)
+      | Ok (Dict _) -> Ok (Some `Dictionary)
+      | Error (`Not_found _) -> Ok None
+      | Error e -> Error e
+    in
+    Lwt.return r
 
   module Reader = struct
     type in_channel = {
@@ -82,41 +98,18 @@ module Make_KV_RO (BLOCK : Mirage_block_lwt.S) = struct
   end
   module HR = Tar.HeaderReader(Lwt)(Reader)
 
-  let connect b =
-    BLOCK.get_info b >>= fun info ->
-    let in_channel = { Reader.b; offset = 0L; info } in
-    let rec loop map =
-      HR.read in_channel >>= function
-      | Result.Error `Eof -> Lwt.return map
-      | Result.Ok tar ->
-        let filename = trim_slash tar.Tar.Header.file_name in
-        let data_tar_offset = Int64.div in_channel.Reader.offset 512L in
-        let map = StringMap.add filename (tar, data_tar_offset) map in
-        Reader.skip in_channel (Int64.to_int tar.Tar.Header.file_size) >>= fun () ->
-        Reader.skip in_channel (Tar.Header.compute_zero_padding_length tar) >>= fun () ->
-        loop map in
-    loop StringMap.empty >>= fun map ->
-    Lwt.return ({ b; map; info })
-
-  let disconnect _ = Lwt.return ()
-
-  let mem t key =
-    Lwt.return (Ok (StringMap.mem key t.map))
-
-  let read t key (offset:int64) (length:int64) =
-    let key = trim_slash key in
-    if not(StringMap.mem key t.map)
-    then Lwt.return (Error (`Unknown_key key))
-    else begin
-      let hdr, start_sector = StringMap.find key t.map in
-
+  let get t key =
+    match get_node t.map key with
+    | Error e -> Lwt.return (Error e)
+    | Ok (Dict _) -> Lwt.return (Error (`Value_expected key))
+    | Ok (Value (hdr, start_sector)) ->
       BLOCK.get_info t.b >>= fun info ->
       let open Int64 in
       let sector_size = of_int info.Mirage_block.sector_size in
 
       (* Compute the unaligned data we need to read *)
-      let start_bytes = add (mul start_sector 512L) offset in
-      let length_bytes = min length (sub hdr.Tar.Header.file_size offset) in
+      let start_bytes = mul start_sector 512L in
+      let length_bytes = hdr.Tar.Header.file_size in
       let end_bytes = add start_bytes length_bytes in
       (* Compute the starting sector and ending sector (rounding down then up) *)
       let start_sector, start_padding = div start_bytes sector_size, rem start_bytes sector_size in
@@ -129,18 +122,97 @@ module Make_KV_RO (BLOCK : Mirage_block_lwt.S) = struct
       (* Don't try to read beyond the end of the archive *)
       let total_size_bytes = Int64.(mul t.info.Mirage_block.size_sectors sector_size) in
       let tmp = Cstruct.sub block 0 (min (Cstruct.len block) Int64.(to_int @@ (sub total_size_bytes (mul start_sector 512L)))) in
-      BLOCK.read t.b start_sector [ tmp ]
-      >>= function
-      | Error _ -> Lwt.fail (Failure (Printf.sprintf "Failed to read %s" key))
-      | Ok () -> Lwt.return (Ok [ Cstruct.sub block (to_int start_padding) (to_int length_bytes) ])
-    end
+      BLOCK.read t.b start_sector [ tmp ] >|= function
+      | Error b -> Error (`Block b)
+      | Ok () ->
+        let buf = Cstruct.sub block (to_int start_padding) (to_int length_bytes) in
+        Ok (Cstruct.to_string buf)
 
-  let size t key =
-    let key = trim_slash key in
-    if not(StringMap.mem key t.map)
-    then Lwt.return (Error (`Unknown_key key))
-    else begin
-      let hdr, _start_sector = StringMap.find key t.map in
-      Lwt.return (Ok hdr.Tar.Header.file_size)
-    end
+  let list t key =
+    let r = match get_node t.map key with
+      | Ok (Dict (_, m)) ->
+        Ok (StringMap.fold (fun key value acc ->
+            match value with
+            | Dict _ -> (key, `Dictionary) :: acc
+            | Value _ -> (key, `Value) :: acc)
+            m [])
+      | Ok (Value _) -> Error (`Dictionary_expected key)
+      | Error e -> Error e
+    in
+    Lwt.return r
+
+  let to_day_ps hdr =
+    let ts =
+      match Ptime.Span.of_float_s (Int64.to_float hdr.Tar.Header.mod_time) with
+      | None -> Ptime.epoch
+      | Some span -> match Ptime.add_span Ptime.epoch span with
+        | None -> Ptime.epoch
+        | Some ts -> ts
+    in
+    Ptime.(Span.to_d_ps (to_span ts))
+
+  let last_modified t key =
+    let r = match get_node t.map key with
+      | Ok (Dict (hdr, _)) -> Ok (to_day_ps hdr)
+      | Ok (Value (hdr, _)) -> Ok (to_day_ps hdr)
+      | Error e -> Error e
+    in
+    Lwt.return r
+
+  let digest t key =
+    get t key >|= function
+    | Error e -> Error e
+    | Ok data -> Ok (Digest.string data)
+
+  (* Compare filenames without a leading / or ./ *)
+  let trim_slash x =
+    let startswith prefix x =
+      let prefix' = String.length prefix in
+      let x' = String.length x in
+      x' >= prefix' && (String.sub x 0 prefix' = prefix) in
+    if startswith "./" x
+    then String.sub x 2 (String.length x - 2)
+    else if startswith "/" x
+    then String.sub x 1 (String.length x - 1)
+    else x
+
+  let is_dict filename =
+    String.get filename (pred (String.length filename)) = '/'
+
+  let insert map key value =
+    let rec go m = function
+      | [] -> assert false
+      | [hd] -> StringMap.add hd value m
+      | hd::tl ->
+        let hdr, m' = match StringMap.find_opt hd m with
+          | None -> Tar.Header.make hd 0L, StringMap.empty
+          | Some (Value _) -> assert false
+          | Some (Dict (hdr, m)) -> hdr, m
+        in
+        let m'' = go m' tl in
+        StringMap.add hd (Dict (hdr, m'')) m
+    in
+    go map (Mirage_kv.Key.segments key)
+
+  let connect b =
+    BLOCK.get_info b >>= fun info ->
+    let in_channel = { Reader.b; offset = 0L; info } in
+    let rec loop map =
+      HR.read in_channel >>= function
+      | Result.Error `Eof -> Lwt.return map
+      | Result.Ok tar ->
+        let filename = trim_slash tar.Tar.Header.file_name in
+        let data_tar_offset = Int64.div in_channel.Reader.offset 512L in
+        let v_or_d = if is_dict filename then Dict (tar, StringMap.empty) else Value (tar, data_tar_offset) in
+        let map = insert map (Mirage_kv.Key.v filename) v_or_d in
+        Reader.skip in_channel (Int64.to_int tar.Tar.Header.file_size) >>= fun () ->
+        Reader.skip in_channel (Tar.Header.compute_zero_padding_length tar) >>= fun () ->
+        loop map in
+    let root = StringMap.empty in
+    loop root >>= fun map ->
+    let map = Dict (Tar.Header.make "/" 0L, map) in
+    Lwt.return ({ b; map; info })
+
+  let disconnect _ = Lwt.return ()
+
 end
