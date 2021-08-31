e6e36e95683663b673887e253e74b443278dcdd4
Antonin Décimo <antonin@tarides.com>
2021-08-31 18:57:35 +0200

    Minor docs fixes
    Signed-off-by: Antonin Décimo <antonin@tarides.com>

--- unix/tar_unix.mli
+++ unix/tar_unix.mli
@@ -14,26 +14,26 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-(** Unix I/O for tar-formatted data *)
+(** Unix I/O for tar-formatted data. *)
 
 val really_read: Unix.file_descr -> Cstruct.t -> unit
 (** [really_read fd buf] fills [buf] with data from [fd] or raises
-    End_of_file *)
+   {!Stdlib.End_of_file}. *)
 
 val really_write: Unix.file_descr -> Cstruct.t -> unit
 (** [really_write fd buf] writes the full contents of [buf] to [fd]
-    or raises End_of_file *)
+    or {!Stdlib.End_of_file}. *)
 
 module Header : sig
   include module type of Tar.Header
 
   (** Returns the next header block or throws End_of_stream if two consecutive
       zero-filled blocks are discovered. Assumes stream is positioned at the
-      possible start of a header block. End_of_file is thrown if the stream
-      unexpectedly fails *)
+      possible start of a header block.
+      @raise Stdlib.End_of_file if the stream unexpectedly fails. *)
   val get_next_header : ?level:compatibility -> Unix.file_descr -> t
 
-  (** Return the header needed for a particular file on disk *)
+  (** Return the header needed for a particular file on disk. *)
   val of_file : ?level:compatibility -> string -> t
 end
 
@@ -44,35 +44,35 @@ val write_block: ?level:Header.compatibility -> Header.t -> (Unix.file_descr -> 
 
 val write_end: Unix.file_descr -> unit
   [@@ocaml.deprecated "Deprecated in favor of Tar.HeaderWriter"]
-  (** Write a stream terminator to [fd]  *)
+  (** Write a stream terminator to [fd]. *)
 
 module Archive : sig
-  (** Utility functions for operating over whole tar archives *)
+  (** Utility functions for operating over whole tar archives. *)
 
   (** Read the next header, apply the function 'f' to the fd and the header. The function
       should leave the fd positioned immediately after the datablock. Finally the function
-      skips past the zero padding to the next header *)
+      skips past the zero padding to the next header. *)
   val with_next_file : Unix.file_descr -> (Unix.file_descr -> Header.t -> 'a) -> 'a
 
-  (** List the contents of a tar *)
+  (** List the contents of a tar. *)
   val list : ?level:Header.compatibility -> Unix.file_descr -> Header.t list
 
   (** [extract dest] extract the contents of a tar.
-      Apply 'dest' on each source filename to know the destination filename *)
+      Apply [dest] on each source filename to change the destination filename. *)
   val extract : (string -> string) -> Unix.file_descr -> unit
 
-  (** Create a tar on file descriptor fd from the filename list 'files' *)
+  (** Create a tar on file descriptor fd from the filename list 'files'. *)
   val create : string list -> Unix.file_descr -> unit
 
-  (** [copy_n ifd odf n] copies exactly [n] bytes from [ifd] to [ofd] *)
+  (** [copy_n ifd odf n] copies exactly [n] bytes from [ifd] to [ofd]. *)
   val copy_n : Unix.file_descr -> Unix.file_descr -> int64 -> unit
   [@@ocaml.deprecated "Deprecated: use your own helper function"]
 
-  (** [multicast_n ?buffer_size ifd ofds n] copies exactly [n] bytes from [ifd] to all [ofds] *)
+  (** [multicast_n ?buffer_size ifd ofds n] copies exactly [n] bytes from [ifd] to all [ofds]. *)
   val multicast_n : ?buffer_size:int -> Unix.file_descr -> Unix.file_descr list -> int64 -> unit
     [@@ocaml.deprecated "Deprecated: use your own helper function"]
 
-  (** [skip fd n] reads and throws away [n] bytes from [fd] *)
+  (** [skip fd n] reads and throws away [n] bytes from [fd]. *)
   val skip : Unix.file_descr -> int -> unit
     [@@ocaml.deprecated "Deprecated: use your own helper function"]
 
