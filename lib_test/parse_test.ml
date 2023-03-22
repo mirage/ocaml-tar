0a2e8e080133a1eef950e467b906cf3e8469125b
Antonin Décimo <antonin@tarides.com>
2023-03-22 13:57:59 +0100

    Replace ounit by Alcotest

--- lib_test/parse_test.ml
+++ lib_test/parse_test.ml
@@ -14,7 +14,6 @@
 
 [@@@warning "-3-27"] (* FIXME: deprecation from the tar library *)
 
-open OUnit2
 open Lwt.Infix
 
 let convert_path os path =
@@ -34,7 +33,14 @@ module Unix = struct
     if Sys.win32 then truncate (convert_path `Windows path) else truncate path
 end
 
-let header _test_ctxt =
+let cstruct = Alcotest.testable
+                (fun f x -> Fmt.pf f "%a" Cstruct.hexdump_pp x)
+                Cstruct.equal
+let pp_header f x = Fmt.pf f "%s" (Tar.Header.to_detailed_string x)
+let header =
+  Alcotest.testable (fun f x -> Fmt.pf f "%a" (Fmt.option pp_header) x) ( = )
+
+let header () =
   (* check header marshalling and unmarshalling *)
   let h = Tar.Header.make ~file_mode:5 ~user_id:1001 ~group_id:1002 ~mod_time:55L ~link_name:"" "hello" 1234L in
   let txt = "hello\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\0000000005\0000001751\0000001752\00000000002322\00000000000067\0000005534\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000" in
@@ -43,16 +49,23 @@ let header _test_ctxt =
   let c' = Cstruct.create Tar.Header.length in
   for i = 0 to Tar.Header.length - 1 do Cstruct.set_uint8 c' i 0 done;
   Tar.Header.marshal c' h;
-  assert_equal ~printer:(fun x -> String.escaped (Cstruct.to_string x)) ~cmp:Cstruct.equal c c';
-  let printer = function
-    | None -> "None"
-    | Some x -> "Some " ^ (Tar.Header.to_detailed_string x) in
-  assert_equal ~printer (Some h) (Tar.Header.unmarshal c');
-  assert_equal ~printer:string_of_int 302 (Tar.Header.compute_zero_padding_length h)
+  Alcotest.(check cstruct) "marshalled headers" c c';
+  Alcotest.(check header) "unmarshalled headers" (Some h) (Tar.Header.unmarshal c');
+  Alcotest.(check int) "zero padding length" 302 (Tar.Header.compute_zero_padding_length h)
 
 let set_difference a b = List.filter (fun a -> not(List.mem a b)) a
 
-let with_tar ?(level:Tar.Header.compatibility option) ?files ?(sector_size = 512) test_ctxt f =
+let with_file ~prefix ~suffix f =
+  let filename = Filename.temp_file prefix suffix in
+  Fun.protect (fun () -> f filename) ~finally:(fun () -> Sys.remove filename)
+
+let with_tmpdir f =
+  let filename = Filename.(temp_file (__FILE__ |> basename |> remove_extension) "") in
+  Sys.remove filename;
+  Unix.mkdir filename 0o700;
+  Fun.protect (fun () -> f filename) ~finally:(fun () -> Unix.rmdir filename)
+
+let with_tar ?(level:Tar.Header.compatibility option) ?files ?(sector_size = 512) f =
   let format = match level with
     | None -> ""
     | Some format -> "--format=" ^ match format with
@@ -61,8 +74,7 @@ let with_tar ?(level:Tar.Header.compatibility option) ?files ?(sector_size = 512
   let files = match files with
     | None -> List.map (fun x -> "lib/" ^ x) (Array.to_list (Sys.readdir "lib"))
     | Some files -> files in
-  let tar_filename, ch = bracket_tmpfile ~prefix:"tar-test" ~suffix:".tar" test_ctxt in
-  close_out ch;
+  with_file ~prefix:"tar-test" ~suffix:".tar" @@ fun tar_filename ->
   let tar_filename = if Sys.win32 then convert_path `Unix tar_filename else tar_filename in
   let tar_block_size = sector_size / 512 in
   let cmdline = Printf.sprintf "tar -cf %s -b %d %s %s" tar_filename tar_block_size format (String.concat " " files) in
@@ -73,22 +85,21 @@ let with_tar ?(level:Tar.Header.compatibility option) ?files ?(sector_size = 512
   end;
   f tar_filename files
 
-let can_read_tar test_ctxt =
-  with_tar test_ctxt
+let can_read_tar () =
+  with_tar
     (fun tar_filename files ->
        let fd = Unix.openfile tar_filename [ Unix.O_RDONLY ] 0 in
        let files' = List.map (fun t -> t.Tar.Header.file_name) (Tar_unix.Archive.list fd) in
        Unix.close fd;
        let missing = set_difference files files' in
        let missing' = set_difference files' files in
-       assert_equal ~printer:(String.concat "; ") [] missing;
-       assert_equal ~printer:(String.concat "; ") [] missing'
+       Alcotest.(check (list string)) "missing" [] missing;
+       Alcotest.(check (list string)) "missing'" [] missing'
     )
 
-let can_write_pax test_ctxt =
+let can_write_pax () =
   let open Tar_unix in
-  let filename, ch = bracket_tmpfile ~prefix:"tar-test" ~suffix:".tar" test_ctxt in
-  close_out ch;
+  with_file ~prefix:"tar-test" ~suffix:".tar" @@ fun filename ->
   (* This userid is too large for a regular ustar header *)
   let user_id = 0x07777777 + 1 in
   (* Write a file which would need a pax header *)
@@ -104,14 +115,12 @@ let can_write_pax test_ctxt =
   Fun.protect
     (fun () ->
       match Archive.list fd with
-      | [ one ] -> assert (one.Tar.Header.user_id = user_id)
-      | xs ->
-        Printf.fprintf stderr "Headers = [ %s ]\n%!" (String.concat "; " (List.map Tar.Header.to_detailed_string xs));
-        assert false
+      | [ one ] -> Alcotest.(check int) "user_id" user_id one.Tar.Header.user_id
+      | xs -> Alcotest.failf "Headers = %a" (Fmt.list pp_header) xs
     ) ~finally:(fun () -> Unix.close fd)
 
 
-let can_list_longlink_tar _test_ctxt =
+let can_list_longlink_tar () =
   let open Tar_unix in
   let fd = Unix.openfile "lib_test/long.tar" [ Unix.O_RDONLY ] 0o0 in
   Fun.protect
@@ -124,7 +133,7 @@ let can_list_longlink_tar _test_ctxt =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/BCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/";
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/BCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/CDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.txt";
       ] in
-      assert_equal ~printer:(String.concat ", ") expected filenames
+      Alcotest.(check (list string)) "respects filenames" expected filenames
     ) ~finally:(fun () -> Unix.close fd)
 
 let starts_with ~prefix s =
@@ -136,13 +145,13 @@ let starts_with ~prefix s =
     else aux (i + 1)
   in len_s >= len_pre && aux 0
 
-let can_transform_tar test_ctxt =
+let can_transform_tar () =
   let level = Tar.Header.Ustar in
-  with_tar ~level test_ctxt (fun tar_in _file_list ->
+  with_tar ~level (fun tar_in _file_list ->
       let fd_in = Unix.openfile tar_in [O_RDONLY] 0 in
       let tar_out = Filename.temp_file "tar-transformed" ".tar" in
       let fd_out = Unix.openfile tar_out [O_WRONLY; O_CREAT] 0o644 in
-      let temp_dir = bracket_tmpdir test_ctxt in
+      with_tmpdir @@ fun temp_dir ->
       Tar_unix.Archive.transform ~level (fun hdr ->
           {hdr with Tar.Header.file_name = Filename.concat temp_dir hdr.file_name})
         fd_in fd_out;
@@ -150,7 +159,8 @@ let can_transform_tar test_ctxt =
       Unix.close fd_out;
       let fd_in = Unix.openfile tar_out [O_RDONLY] 0 in
       Tar_unix.Archive.with_next_file fd_in (fun _fd_in hdr ->
-          assert_bool "Filename was transformed" (starts_with ~prefix:temp_dir hdr.file_name)))
+          Alcotest.(check bool) "Filename was transformed" true (starts_with ~prefix:temp_dir hdr.file_name));
+      Unix.close fd_in)
 
 module Block4096 = struct
   include Block
@@ -179,7 +189,7 @@ module B = struct
 end
 
 module Test(B: BLOCK) = struct
-  let add_data_to_tar ?(level:Tar.Header.compatibility option) ?files test_ctxt f =
+  let add_data_to_tar ?(level:Tar.Header.compatibility option) ?files switch () f =
     let f tar_filename files =
       let size = Unix.(stat tar_filename).st_size in
       let size = B.sector_size * ((pred size + 4096 + B.sector_size) / B.sector_size) in
@@ -194,9 +204,9 @@ module Test(B: BLOCK) = struct
       let files = "barf" :: files in
       f tar_filename files
     in
-    with_tar ?level ?files ~sector_size:B.sector_size test_ctxt f
+    with_tar ?level ?files ~sector_size:B.sector_size f
 
-  let add_more_data_to_tar ?(level:Tar.Header.compatibility option) ?files test_ctxt f =
+  let add_more_data_to_tar ?(level:Tar.Header.compatibility option) ?files switch () f =
     let f tar_filename files =
       let size = Unix.(stat tar_filename).st_size in
       (* Add 4 KB rounding up to block size *)
@@ -216,9 +226,9 @@ module Test(B: BLOCK) = struct
       let files = "barf" :: "barf2" :: files in
       f tar_filename files
     in
-    with_tar ?level ?files ~sector_size:B.sector_size test_ctxt f
+    with_tar ?level ?files ~sector_size:B.sector_size f
 
-  let write_with_full_archive ?(level:Tar.Header.compatibility option) ?files test_ctxt =
+  let write_with_full_archive ?(level:Tar.Header.compatibility option) ?files switch () =
     let f tar_filename files =
       B.connect tar_filename >>= fun b ->
       let module KV_RW = Tar_mirage.Make_KV_RW(Pclock)(B) in
@@ -227,7 +237,7 @@ module Test(B: BLOCK) = struct
       | Error `No_space -> Lwt.return ()
       | _ -> failwith "expected `No_space"
     in
-    with_tar ?level ?files test_ctxt f
+    with_tar ?level ?files f
 
   let check_tar tar_filename files =
     B.connect tar_filename >>= fun b ->
@@ -250,7 +260,7 @@ module Test(B: BLOCK) = struct
                   let (_: int) = Unix.lseek fd ofs Unix.SEEK_SET in
                   let buf = Bytes.make len '\000' in
                   let len' = Unix.read fd buf 0 len in
-                  assert_equal ~printer:string_of_int len len';
+                  Alcotest.(check int) "same length" len len';
                   Bytes.to_string buf
                ) ~finally:(fun () -> Unix.close fd) in
          let read_tar key =
@@ -260,50 +270,56 @@ module Test(B: BLOCK) = struct
          (* Read whole file *)
          let value = read_file file 0 (Int64.to_int size) in
          read_tar (Mirage_kv.Key.v file) >>= fun value' ->
-         assert_equal ~printer:(fun x -> x) value value';
+         Alcotest.(check string) "same content" value value';
          if Int64.compare size 2L = 1 then begin
            let value = read_file file 1 ((Int64.to_int size) - 2) in
            read_tar (Mirage_kv.Key.v file) >>= fun value' ->
            let value'' = String.sub value' 1 ((Int64.to_int size) - 2) in
-           assert_equal ~printer:(fun x -> x) value value'';
+           Alcotest.(check string) "same content" value value'';
            Lwt.return_unit
          end else Lwt.return_unit
       ) files
 
-  let can_read_through_BLOCK ?files test_ctxt =
-    with_tar ?files ~sector_size:B.sector_size test_ctxt check_tar
+  let can_read_through_BLOCK ~files switch () =
+    with_tar ~files ~sector_size:B.sector_size check_tar
 
-  let write_test test_ctxt =
-     add_data_to_tar test_ctxt check_tar
+  let write_test switch () =
+     add_data_to_tar switch () check_tar
 
-  let write_more_test test_ctxt =
-     add_more_data_to_tar test_ctxt check_tar
+  let write_more_test switch () =
+     add_more_data_to_tar switch () check_tar
 
-  let check_not_padded test_ctxt =
+  let check_not_padded switch () =
     Unix.openfile "empty" [ Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC ] 0o644 |> Unix.close;
-    can_read_through_BLOCK ~files:["empty"] test_ctxt
+    can_read_through_BLOCK ~files:["empty"] switch ()
 end
 
 module Sector512 = Test(B)
 module Sector4096 = Test(Block4096)
 
 let () =
-  let suite = "tar" >:::
-              [
-                "header" >:: header;
-                "can_read_tar" >:: can_read_tar;
-                "can_read_through_BLOCK/512" >:: OUnitLwt.lwt_wrapper Sector512.can_read_through_BLOCK;
-                "not 4KiB padded" >:: OUnitLwt.lwt_wrapper Sector512.check_not_padded;
-                "can_read_through_BLOCK/4096" >:: OUnitLwt.lwt_wrapper Sector4096.can_read_through_BLOCK;
-                "can write pax headers" >:: can_write_pax;
-                "can read @Longlink" >:: can_list_longlink_tar;
-                "can transform tars" >:: can_transform_tar;
-                "add_data_to_tar BLOCK/512" >:: OUnitLwt.lwt_wrapper Sector512.write_test;
-                "add_more_data_to_tar BLOCK/512" >:: OUnitLwt.lwt_wrapper Sector512.write_more_test;
-                "write_with_full_archive BLOCK/512" >:: OUnitLwt.lwt_wrapper Sector512.write_with_full_archive;
-                "add_data_to_tar BLOCK/4096" >:: OUnitLwt.lwt_wrapper Sector4096.write_test;
-                "add_more_data_to_tar BLOCK/4096" >:: OUnitLwt.lwt_wrapper Sector4096.write_more_test;
-              ] in
+  let ( >:: ) desc f = Alcotest.test_case desc `Quick f in
+  let suite = "parse_test", [
+      "header" >:: header;
+      "can_read_tar" >:: can_read_tar;
+      "can write pax headers" >:: can_write_pax;
+      "can read @Longlink" >:: can_list_longlink_tar;
+      "can transform tars" >:: can_transform_tar;
+    ]
+  in
+  let ( >:: ) desc f = Alcotest_lwt.test_case desc `Quick f in
+  let lwt_suite = "parse_test-lwt", [
+      "can_read_through_BLOCK/512" >:: Sector512.can_read_through_BLOCK ~files:[];
+      "not 4KiB padded" >:: Sector512.check_not_padded;
+      "can_read_through_BLOCK/4096" >:: Sector4096.can_read_through_BLOCK ~files:[];
+      "add_data_to_tar BLOCK/512" >:: Sector512.write_test;
+      "add_more_data_to_tar BLOCK/512" >:: Sector512.write_more_test;
+      "write_with_full_archive BLOCK/512" >:: Sector512.write_with_full_archive;
+      "add_data_to_tar BLOCK/4096" >:: Sector4096.write_test;
+      "add_more_data_to_tar BLOCK/4096" >:: Sector4096.write_more_test;
+    ]
+  in
   (* pwd = _build/default/lib_test *)
   Unix.chdir "../../..";
-  run_test_tt_main suite
+  Alcotest.run "parse-test" [suite];
+  Lwt_main.run @@ Alcotest_lwt.run "parse-test-lwt" [lwt_suite]
